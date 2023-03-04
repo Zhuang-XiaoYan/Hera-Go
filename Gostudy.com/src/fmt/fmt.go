d0086f831f0d3880381aacf1de036977359169c0
xjl <18279148786@163.com>
2023-03-04 08:05:13 +0800

    feat(Hera-Go) [xjl012] 更新README.md

--- Gostudy.com/src/fmt/fmt.go
+++ Gostudy.com/src/fmt/fmt.go
@@ -47,7 +47,6 @@ func bufioDemo() {
 // func Fscan(r io.Reader, a ...interface{}) (n int, err error)
 // func Fscanln(r io.Reader, a ...interface{}) (n int, err error)
 // func Fscanf(r io.Reader, format string, a ...interface{}) (n int, err error)
-
 // 这几个函数功能分别类似于fmt.Scan、fmt.Scanf、fmt.Scanln三个函数，只不过它们不是从标准输入中读取数据而是从指定字符串中读取数据。
 // func Sscan(str string, a ...interface{}) (n int, err error)
 // func Sscanln(str string, a ...interface{}) (n int, err error)
